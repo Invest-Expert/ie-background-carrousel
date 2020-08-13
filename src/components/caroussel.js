f1f43dcc7656784114bde67c403c2c6b1f74ac48
Taric Santos de Andrade <taric@z2h.io>
2020-08-13 12:50:49 -0700

    Bootstrap carrousel

---
+++ src/components/caroussel.js
@@ -0,0 +1,76 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Carousel from 'react-bootstrap/Carousel'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import bg01 from "../images/background-01.png"
+import bg02 from "../images/background-02.png"
+import bg03 from "../images/background-03.png"
+import bg04 from "../images/background-04.png"
+import bg05 from "../images/background-05.png"
+
+
+const CarrouselImg = ({ children }) => (
+    <div background-color={'Light'}>
+    <Carousel>
+        
+      <Carousel.Item>      
+        <img
+          className="d-block w-100"
+          src={bg01}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={bg02}
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={bg03}
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={bg04}
+          alt="Fourth slide"
+        />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={bg05}
+          alt="Fitht slide"
+        />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+    </div>
+
+)  
+
+export default CarrouselImg  
