9f994f87b6fd411fcdb144fec16400960d893523
Taric Santos de Andrade <taric@z2h.io>
2020-08-12 17:39:31 -0700

    Update config
    Fix erro to deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,13 +13,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `backgrounds`,
-      path: `${__dirname}/src/images/bg`, // wherever background images are stored
-    },
-  },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
